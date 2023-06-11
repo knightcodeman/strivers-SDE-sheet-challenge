c241bdf8bf06336dd9084bab25ff66f73cf334c4
knightcodeman <136008661+knightcodeman@users.noreply.github.com>
2023-06-11 22:56:01 +0530

    4_kadane'salgorithm.cpp
    Problem_NO. 4

---
+++ 4_kadane'salgorithm.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    
+        long long sum=0;
+        long long maxsum=0;
+        for(int i=0;i<n;i++)
+        {
+           sum+=arr[i];
+           maxsum=max(maxsum,sum);
+           if(sum<0)sum=0;
+        }
+        return  maxsum;
+    
+}
