3da38dac6d31502ea21d13732164f667ecb25766
knightcodeman <136008661+knightcodeman@users.noreply.github.com>
2023-06-17 11:58:21 +0530

    13_modular_exponentiation.cpp
    Problem_no.13

---
+++ 13_modular_exponentiation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int modularExponentiation(int x, int n, int m) {
+    int ans=1;
+	while(n)
+	{
+		if(n&1)
+		{
+       ans=(ans*1LL*x)%m;
+		}
+		x=(x*1LL*x)%m;
+		n>>=1;
+	}
+	return ans;
+}
