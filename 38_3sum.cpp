0a3b52e5ea66678337a058a7714f69daf2d7ea70
knightcodeman <136008661+knightcodeman@users.noreply.github.com>
2023-06-22 14:45:37 +0530

    38_3sum.cpp
    problem_no.38

--- 38_3sum.cpp
+++ 38_3sum.cpp
@@ -6,20 +6,21 @@ vector<vector<int>>findtarget(vector<int>arr,int target,int low,int element)
 	int high=arr.size()-1;
 	while(low<high)
 	{
-      if (arr[high] + arr[low] == target) {
-		      	 vector<int>temp;
-			      int l=arr[low];
-			      int h=arr[high];
-        
-			      temp.push_back(element);
+          if (arr[high] + arr[low] == target) {
+	    vector<int>temp;
+	
+	    temp.push_back(element);
             temp.push_back(l);
             temp.push_back(h);
-		      	ans.push_back(temp);
-        
-            while(low<high && l==arr[low])low++;
-		      	while(low<high && h==arr[high])high--;
-       } 
-		  else if (arr[high] + arr[low] > target)
+	    ans.push_back(temp);
+		  
+            while(low<high && arr[low+1]==arr[low])low++;
+	    while(low<high && arr[high-1]==arr[high])high--;
+		  
+	    low++;
+	    high--;
+          } 
+         else if (arr[high] + arr[low] > target)
             high--;
           else
             low++;
@@ -27,11 +28,12 @@ vector<vector<int>>findtarget(vector<int>arr,int target,int low,int element)
 	return ans;
 }
 vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
-	vector<vector<int>>ans;
-     sort(arr.begin(),arr.end());
+	 vector<vector<int>>ans;
+         sort(arr.begin(),arr.end());
 	 for(int i=0;i<n-2;i++)
 	 {
 		 if(i>0 && arr[i]==arr[i-1])continue;
+		 
 		 int target=K-arr[i];
 		 vector<vector<int>>temp=findtarget(arr,target,i+1,arr[i]);
 		 for(auto it:temp)
