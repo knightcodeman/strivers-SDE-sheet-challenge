2e91899adbbae4183ec07d519bc6c0f66e23e8fb
knightcodeman <136008661+knightcodeman@users.noreply.github.com>
2023-06-26 10:35:11 +0530

    41_remove_duplicates.cpp
    problem_no.41

--- 41_remove_duplicates.cpp
+++ 41_remove_duplicates.cpp
@@ -3,11 +3,11 @@ int removeDuplicates(vector<int> &arr, int n) {
 	for(int i=1;i<n;i++)
 	{
          if(arr[i]==arr[i-1])
-		     {
-			     arr.erase(arr.begin()+i);
-			     n--;
-			     i--;
-		    }
+	{
+	   arr.erase(arr.begin()+i);
+	   n--;
+	   i--;
+	 }
 	}
 	return arr.size();
 }
