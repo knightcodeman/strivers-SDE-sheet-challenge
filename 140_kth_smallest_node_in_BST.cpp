72b8bb488672b2c35ae0ccbf748b4c6f0a6a04e7
Harsh <136008661+knightcodeman@users.noreply.github.com>
2023-07-10 11:32:43 +0530

    140_kth_smallest_node_in_BST.cpp
    problem_no.140

---
+++ 140_kth_smallest_node_in_BST.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+void helper(TreeNode<int>*root,int&k,int &ans){
+    if( k==0 || !root)return;
+    helper(root->left,k,ans);
+    k--;
+    if (k == 0) {
+      ans = root->data;
+      return;
+    }
+    helper(root->right,k,ans);
+
+}
+int kthSmallest(TreeNode<int> *root, int k)
+{
+	int ans=0;
+    helper(root,k,ans);
+    return ans;
+}
